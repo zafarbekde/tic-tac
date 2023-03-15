cc51d38b4a8b78ed74dde9d536aeae804f96a7ab
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-15 15:22:17 +0500

    Update Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import {Box} from './Box'
-import './Board.css'
+import React from 'react'
 
-export const Board = ({board, onClick}) => {
-    
-    return(
-        <div className="board">
-            {
-                board.map((value, idx) => {
-                    return <Box value={value} onClick={() => onClick(idx)}/>
-                })
-            }            
-            
-            </div>
-    )
+import { Box } from "./Box"
+import "./Board.css"
+
+export const Board = ({ board, onClick }) => {
+  return (
+    <div className="board">
+      {
+        board.map((value, idx) => {
+          return <Box value={value} onClick={() => value === null && onClick(idx)} />;
+        })
+      }
+    </div>
+  )
 }
