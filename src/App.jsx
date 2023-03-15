badf3a4fa5590fe5c7233cf23b77a67cec76bc05
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-15 14:40:18 +0500

    korgan kot

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import {Box} from './components/Box.jsx'
 import './App.css';
 
 function App() {
+  const board = ["X","X","X","X","X","X","X","X","X"]
   return (
     <div className="App">
-      
+      <Box value='X' onClick={null}/>
     </div>
   );
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Box} from '../components/Box.css'
+import './Box.css'
 
 export const Box = ({ value, onClick}) => {
     const style = value === "X" ? 'box x' : 'box o'
     return(
-        <button className={style}>x</button>
+        <button className={style} oncClick={onClick}>{value}</button>
     )
 }
+
